85b1a01ff048cdb00a9beb85308a5fb11acaf620
Hideto Ishibashi <hideto@zerobase.jp>
2019-04-11 01:50:22 +0900

    npm install localtunnel

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,34 @@
 import * as Express from 'express';
+import * as localtunnel from 'localtunnel';
 
 const app = Express();
+const port = 3000;
 
-app.get(
-    '/',
-    (req: Express.Request, res: Express.Response) => {
-        return res.send('Hello world.');
-    });
+app.post(
+  '/',
+  (req: Express.Request, res: Express.Response) => {
+    return res.send('Hello world.');
+  }
+);
 
 app.listen(
-    3000,
-    () => {
-        console.log('Example app listening on port 3000!');
-    });
+  port,
+  () => {
+    console.log(`Example app listening on port ${port}!`);
+  }
+);
+
+const tunnel = localtunnel(port, function(err, tunnel) {
+  if (err) {
+    console.log(err);
+    throw err;
+  }
+  console.log(`localtunnel ${tunnel.url} is open.`)
+  tunnel.url;
+});
+
+tunnel.on('close', function() {
+  console.log('localtunnel ${this.url} is closed.')
+});
 
 export default app;
