9e726bd205d3eacec5351eb1d2052dbafc310267
Hideto Ishibashi <hideto@zerobase.jp>
2019-04-17 14:15:51 +0900

    Ensure localtunnel close at exit

--- src/app.ts
+++ src/app.ts
@@ -3,41 +3,40 @@ import {Tunnel} from './tunnel';
 import {Clasp} from './clasp';
 import {GasRepl} from './gas-repl';
 import * as getPort from 'get-port';
+import {EventEmitter} from 'events';
 
 export class App {
+  private event;
   private server;
   private tunnel;
   private clasp;
   private gasRepl;
 
   constructor() {
-    this.server = new Server;
-    this.clasp = new Clasp;
-    this.tunnel = new Tunnel(this.clasp);
-    this.gasRepl = new GasRepl(this.clasp);
+    this.event = new EventEmitter;
+    this.server = new Server(this.event);
+    this.clasp = new Clasp(this.event);
+    this.tunnel = new Tunnel(this.event, this.clasp);
+    this.gasRepl = new GasRepl(this.event, this.clasp);
   }
 
   start() {
-    try {
-      getPort()
-      .then(port => {
-        this.server.start(port)
-        .then(event => {
-          this.tunnel.start(port)
-          .then(tunnel => {
-            this.gasRepl.start(event)
-            .then(repl => this.clasp.start(tunnel.url))
-          });
+    getPort()
+    .then(port => {
+      this.server.start(port)
+      .then(express => {
+        this.tunnel.start(port)
+        .then(tunnel => {
+          this.gasRepl.start()
+          .then(repl => this.clasp.start(tunnel.url))
         });
-      })
-    }
-    catch (err) {
-      if (/REPL Exit/.test(err)) {
-        console.log('kill clasp 2');
-        this.clasp.kill();
-      }
-      console.log(err);
-      throw err;
-    }
+      });
+    });
+    this.event.on('exit', (msg) => {
+      console.log(msg);
+      this.clasp.kill();
+      this.tunnel.close();
+      process.exit();
+    });
   }
 }
--- src/gas-repl.ts
+++ src/gas-repl.ts
@@ -3,25 +3,31 @@ import {EventEmitter} from 'events';
 
 export class GasRepl {
   private clasp;
+  private event: EventEmitter;
 
-  constructor(clasp) {
+  constructor(event: EventEmitter, clasp) {
+    this.event = event;
     this.clasp = clasp;
   }
 
-  start(event: EventEmitter): Promise<EventEmitter> {
+  start(): Promise<EventEmitter> {
     return new Promise((resolve, reject) => {
       //event.once('result', (result) => { /* ignore the first */});
       const replServer = repl.start({
         prompt: '> ',
         eval: (cmd, context, filename, callback) => {
+<<<<<<< HEAD
           event.once('result', (result) => callback(null, result));
           event.emit('input', cmd.trim());
+=======
+          this.event.emit('input', cmd.trim());
+          this.event.once('result', (result) => callback(null, result));
+>>>>>>> e3402d2... Ensure localtunnel close at exit
         }
       });
       replServer.setupHistory('.gas-repl.history', (err, repl) => {});
       replServer.on('exit', () => {
-        this.clasp.kill();
-        process.exit();
+        this.event.emit('exit', 'REPL exit.')
       });
       resolve(replServer);
     });
