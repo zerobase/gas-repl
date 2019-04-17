45d636fd0441565cb0486bf08a5d2cc706a35559
Hideto Ishibashi <hideto@zerobase.jp>
2019-04-17 13:48:41 +0900

    Kill clasp process on exit #2

--- src/tunnel.ts
+++ src/tunnel.ts
@@ -2,6 +2,12 @@ import * as localtunnel from 'localtunnel';
 import {EventEmitter} from 'events';
 
 export class Tunnel {
+  private clasp;
+
+  constructor(clasp) {
+    this.clasp = clasp;
+  }
+
   start(port: number): Promise<EventEmitter> {
     return new Promise((resolve, reject) => {
       const tunnel = localtunnel(port, (err, tunnel) => {
@@ -14,11 +20,13 @@ export class Tunnel {
 
       tunnel.on('close', () => {
         console.log('localtunnel ${this.url} is closed.');
+        this.clasp.kill();
         throw 'localtunnel is closed';
       });
 
       tunnel.on('error', (error) => {
         console.log(`localtunnel error: ${error}`);
+        this.clasp.kill();
         throw error;
       })
     });
